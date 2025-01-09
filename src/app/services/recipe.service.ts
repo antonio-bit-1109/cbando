73b9189be5a424b9f66eb1487fe60fa759d5e0b4
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-01-09 19:48:30 +0100

    add disabilitation card

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { RECIPES } from '../mocks/recipes.mock';
-import { Observable , of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IRecipe } from '../models/recipes.model';
 
+// nel service vengono fatte le chiamate al backend
+// il servizio viene iniattato li dove un componente ne ha bisogno.
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  constructor() {}
 
-  constructor() { }
-
-  //metodo che ritorna un observable 
-  // dal componente posso sottoscrivermi all observable 
-  getRecipes() : Observable<IRecipe[]> {
-    return of(RECIPES)
+  //metodo che ritorna un observable
+  // dal componente posso sottoscrivermi all observable
+  getRecipes(): Observable<IRecipe[]> {
+    return of(RECIPES);
   }
 }
