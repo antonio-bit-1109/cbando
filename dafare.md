c9309d5183565e9be5b14b10bfd69b1db46ee960
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-01-14 21:44:17 +0100

    gg le validation sono da sistemare ma sto cotto dio suino

--- dafare.md
+++ dafare.md
@@ -3,3 +3,4 @@
 - [] implementa il routing innestato 'semplice' anche nel progetto personale
   -[] importa nella home le ultime ricette visualizzate ??
 - [] controlla di nuovo come funzionano import-output ed event emitter
+- [] aggiungi evento al change per il controllo del conferma password
--- src/app/components/user/registration-reactive-form/registration-reactive-form.component.ts
+++ src/app/components/user/registration-reactive-form/registration-reactive-form.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Password } from 'primeng/password';
 
 @Component({
   selector: 'app-registration-reactive-form',
@@ -14,12 +13,31 @@ export class RegistrationReactiveFormComponent {
   public form = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){6,16}$/ // password che deve contenere char speciale lunga da 8 a 16 char
+      ),
+    ]),
     ripetiPassword: new FormControl('', [Validators.required]),
     accetto: new FormControl(false, [Validators.requiredTrue]),
   });
 
   public onSubmit() {
-    console.log(this.form.value);
+    console.log(this.form);
+  }
+
+  public isConfermaPasswordMatch(form: FormGroup) {
+    const password = form.get('password').value;
+    const confermaPassword = form.get('ripetiPassword').value;
+
+    if (password !== confermaPassword) {
+      return false;
+    }
+    return true;
+  }
+
+  public isFormvalid(form: FormGroup) {
+    return form.valid;
   }
 }
